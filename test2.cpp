43804f6f205334d739401444da145a3579037c87
Vedant Shirude <vedant.shirude24@imperial.ac.uk>
2025-08-10 19:03:25 +0100

    Updated gitignore file and fixed CMake file cpr library detection bug

--- test2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Goodbye, world!" << std::endl;
-    std::cout << "This is our first attempt at making a trading strategy simulator" << std::endl;
-    return 0;
-}
